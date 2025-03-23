44ab40e16de161783ece2b5f70af1c5caa798b22
oscherrero <36818091+oscherrero@users.noreply.github.com>
2025-03-23 16:08:55 +0100

    1

--- dev/script.js
+++ dev/script.js
@@ -8,6 +8,10 @@ const TEMAS=[
   [ "cyan", "cyan", "green", "white"  ],
 ]
 const PATH=window.location.href 
+if (PATH.endsWith("/")) {
+  PATH = PATH.slice(0, -1);
+}
+
 const APP=PATH.substring(0, PATH.lastIndexOf("/"))
 
 const NUMTEMAS=TEMAS.length
