1c1f139760b2de4f0efb419927cc55e7d37d06f9
oscherrero <36818091+oscherrero@users.noreply.github.com>
2025-03-23 16:20:33 +0100

    11

--- dev/script.js
+++ dev/script.js
@@ -12,7 +12,7 @@ if (PATH.endsWith("/")) {
   PATH = PATH.slice(0, -1);
 }
 
-const APP=PATH.substring(PATH.lastIndexOf("/"),99)
+const APP=PATH.substring(PATH.lastIndexOf("/")+1,99)
 
 const NUMTEMAS=TEMAS.length
 
