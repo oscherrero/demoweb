b708e1657166cadd256bbbd61a801134c251d556
oscherrero <36818091+oscherrero@users.noreply.github.com>
2025-03-23 16:52:27 +0100

    1

--- dev/script.js
+++ dev/script.js
@@ -135,7 +135,7 @@ function cambiarTema() {
 function aplicarTema(incrementar) { 
   var tema=localStorage.getItem(APP+"tema")||0;
   if (incrementar) {
-    tema = tema < (TEMAS.length-1) ? tema:;
+    tema = tema < (TEMAS.length-1) ? tema:0;
   }
   localStorage.setItem(APP+"tema",tema);
   const root = document.documentElement;
